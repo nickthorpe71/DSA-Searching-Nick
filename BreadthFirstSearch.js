2118e7a46b5e521171620b4f72648e22e97aeba3
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-26 10:31:45 -0500

    adds singly linked list queue

---
+++ BreadthFirstSearch.js
@@ -0,0 +1,15 @@
+import SLL_Queue from './SLL_Queue.js';
+
+export default class BinarySearchTree {
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  bfs(tree, values = []) {
+
+  }
+}
