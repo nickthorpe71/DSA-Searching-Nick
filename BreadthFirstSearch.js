dec200e48448eac92bbb4c71e518e6569f74f383
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-26 10:35:11 -0500

    adds beadth first search

--- BreadthFirstSearch.js
+++ BreadthFirstSearch.js
@@ -10,6 +10,19 @@ export default class BinarySearchTree {
   }
 
   bfs(tree, values = []) {
+    const queue = new SLL_Queue();
+    const node = tree.root; //need to find out who to pass in tree
+    queue.enqueue(node);
+    while (queue.length) {
+      const node = queue.dequeue();
+      values.push(node.value);
 
+      if (node.left)
+        queue.enqueue(node.left);
+
+      if (node.right)
+        queue.enqueue(node.right);
+    }
+    return values;
   }
 }
